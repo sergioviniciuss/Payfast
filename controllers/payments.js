447cdd8a78f930ababc1483d60df310f56cf305d
sergioviniciuss <sergioviniciuss@gmail.com>
2017-06-24 20:03:42 -0300

    Implementation the POST for payment

--- controllers/payments.js
+++ controllers/payments.js
@@ -1,7 +1,13 @@
 module.exports = function(app) {
 	app.get('/payments', function(req, res) {
 		console.log('Received Test request on port 3000');
-		res.send("ok");
+		res.send("OK.");
+	});
+
+	app.post('/payments/payment', function(req, res){
+		var payment = req.body;
+		console.log(payment);
+		res.send("OK.")
 	});
 	
 }
